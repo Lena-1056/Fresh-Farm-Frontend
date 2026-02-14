00eb2891b2c911ed5adc0167a9255fddfd351278
Lena <m.lenadeeshreddy@gmail.com>
2026-02-14 10:50:08 +0530

    feat: add product model

---
+++ lib/models/product_model.dart
@@ -0,0 +1,31 @@
+import 'dart:typed_data';
+
+class ProductModel {
+  final String name;
+  final String category;
+  final double price;
+  final int quantity;
+  final String quantityType;
+  final String location;
+
+  final String description;
+  final bool isOrganic;
+  final String harvestDate;
+
+  final Uint8List? webImage;
+  final String? imagePath;
+
+  ProductModel({
+    required this.name,
+    required this.category,
+    required this.price,
+    required this.quantity,
+    required this.quantityType,
+    required this.location,
+    required this.description,
+    required this.isOrganic,
+    required this.harvestDate,
+    this.webImage,
+    this.imagePath,
+  });
+}
